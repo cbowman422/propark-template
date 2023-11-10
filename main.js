5fb50bed15d1fcdd6f835f4eeacfc5b6bb8a5004
Christopher Bowman <116463279+cbowman422@users.noreply.github.com>
2023-11-09 21:49:07 -0500

    locations and footer testimonial

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+const useState = (defaultValue) => {
+  let value = defaultValue;
+  const getValue = () => value
+  const setValue = newValue => value = newValue
+  return [getValue, setValue];
+}
+
+const [counter, setCounter] = useState(0);
+
+console.log(counter());
