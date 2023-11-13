43f988c5a017d5ccdc4ce4e8207588f5f08e2995
Christopher Bowman <116463279+cbowman422@users.noreply.github.com>
2023-11-13 16:41:14 -0500

    functionality for services with box shadow

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let mainServicesButton1 = document.getElementById('mainServicesButton1');
 let mainServicesButton2 = document.getElementById('mainServicesButton2');
 let mainServicesButton3 = document.getElementById('mainServicesButton3');
 
-mainServicesButton1.addEventListener('click', updateServices1());
+mainServicesButton1.addEventListener('click', updateServices1);
 mainServicesButton2.addEventListener('click', updateServices2);
 mainServicesButton3.addEventListener('click', updateServices3);
 
@@ -24,19 +24,25 @@ function toggleMenu(){
 };
 
 function updateServices1(){
-  servicesInfo("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeuLh5RyUR6FDYZi9MQeD7RoQfU90cY67quA&usqp=CAU","Shuttle Transportation","Our transportation services are diverse and tailored to your needs, offering a range of employer shuttle sizes, from single vehicles on a five-day workweek to 24/7 availability. In non-emergency medical transportation, our suite includes courtesy, patient, and medical center parking shuttles.");
+  servicesInfo("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeuLh5RyUR6FDYZi9MQeD7RoQfU90cY67quA&usqp=CAU","Shuttle Transportation","Our transportation services are diverse and tailored to your needs, offering a range of employer shuttle sizes, from single vehicles on a five-day workweek to 24/7 availability. In non-emergency medical transportation, our suite includes courtesy, patient, and medical center parking shuttles.","mainServicesButton1","mainServicesButton2","mainServicesButton3");
 }
 
 function updateServices2(){
-  servicesInfo("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeuLh5RyUR6FDYZi9MQeD7RoQfU90cY67quA&usqp=CAU","Parking Management","Our Parking Management are diverse and tailored to your needs, offering a range of employer shuttle sizes, from single vehicles on a five-day workweek to 24/7 availability. In non-emergency medical transportation, our suite includes courtesy, patient, and medical center parking shuttles.");
+  servicesInfo("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeuLh5RyUR6FDYZi9MQeD7RoQfU90cY67quA&usqp=CAU","Parking Management","Our Parking Management are diverse and tailored to your needs, offering a range of employer shuttle sizes, from single vehicles on a five-day workweek to 24/7 availability. In non-emergency medical transportation, our suite includes courtesy, patient, and medical center parking shuttles.","mainServicesButton2","mainServicesButton1","mainServicesButton3");
 }
 
 function updateServices3(){
-  servicesInfo("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeuLh5RyUR6FDYZi9MQeD7RoQfU90cY67quA&usqp=CAU","Event Parking","Our Event Parking services are diverse and tailored to your needs, offering a range of employer shuttle sizes, from single vehicles on a five-day workweek to 24/7 availability. In non-emergency medical transportation, our suite includes courtesy, patient, and medical center parking shuttles.");
+  servicesInfo("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeuLh5RyUR6FDYZi9MQeD7RoQfU90cY67quA&usqp=CAU","Event Parking","Our Event Parking services are diverse and tailored to your needs, offering a range of employer shuttle sizes, from single vehicles on a five-day workweek to 24/7 availability. In non-emergency medical transportation, our suite includes courtesy, patient, and medical center parking shuttles.","mainServicesButton3","mainServicesButton1","mainServicesButton2");
 }
 
-function servicesInfo(servicesImage, servicesName, servicesText){
+// Call first update services function on page load
+updateServices1();
+
+function servicesInfo(servicesImage, servicesName, servicesText, mainButtonShadow, mainButtonShadowRemove1,mainButtonShadowRemove2 ){
   document.getElementById("main-services-img").src = servicesImage;
   document.getElementById("main-services-h3").innerHTML = servicesName;
   document.getElementById("main-services-p").innerHTML = servicesText;
+  document.getElementById(mainButtonShadow).style.boxShadow = "2px 2px 2px 1px";
+  document.getElementById(mainButtonShadowRemove1).style.boxShadow = "none";
+  document.getElementById(mainButtonShadowRemove2).style.boxShadow = "none";
 }
